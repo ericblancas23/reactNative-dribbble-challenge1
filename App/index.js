d6d7e6b0db8398e5c14216bc3df086bdfe4ca0b3
Eric Blancas <eblancas@eblancas.attlocal.net>
2020-01-20 20:12:42 -0500

    animation completed

--- App/index.js
+++ App/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
 import "../assets/bg.jpg";
-import Animated from 'react-native-reanimated';
+import Animated, { Easing } from 'react-native-reanimated';
 import { TapGesture, State, TapGestureHandler } from 'react-native-gesture-handler';
 
 const {width,height} = Dimensions.get("window")
-const { Value,event,block,eq,cond,set } = Animated;
+const { Value,event,block,eq,cond,set, Clock, startClock, stopClock,
+        debug, timing, clockRunning, interpolate, Extrapolate } = Animated;
+
+
+function runTiming(clock, value, dest) {
+    const state = {
+      finished: new Value(0),
+      position: new Value(0),
+      time: new Value(0),
+      frameTime: new Value(0)
+    };
+  
+    const config = {
+      duration: 1000,
+      toValue: new Value(0),
+      easing: Easing.inOut(Easing.ease)
+    };
+  
+    return block([
+      cond(clockRunning(clock), 0, [
+        set(state.finished, 0),
+        set(state.time, 0),
+        set(state.position, value),
+        set(state.frameTime, 0),
+        set(config.toValue, dest),
+        startClock(clock)
+      ]),
+      timing(clock, state, config),
+      cond(state.finished, debug('stop clock', stopClock(clock))),
+      state.position
+    ]);
+  }
 
 
 export default class MusicApp extends Component {
@@ -16,28 +47,43 @@ export default class MusicApp extends Component {
         this.onStateChange = event([
             {
                 nativeEvent: ( { state } ) => block([
-                    cond(eq(state, State.END), set(this.buttonOpacity, 0))
+                    cond(eq(state, State.END), set(this.buttonOpacity, 
+                        runTiming(new Clock(), 1, 0)))
                 ])
             }
         ])
+        this.buttonY = interpolate(this.buttonOpacity, {
+            inputRange: [0,1],
+            outputRange: [100,0],
+            extrapolate: Extrapolate.CLAMP,
+        });
+
+        this.bgY = interpolate(this.buttonOpacity, {
+            inputRange: [0,1],
+            outputRange: [-height / 3,0],
+            extrapolate: Extrapolate.CLAMP,
+        }); 
     }
 
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: "white", justifyContent: "flex-end" }}>
-                <View style={{...StyleSheet.absoluteFill}}>
+                <Animated.View style={{...StyleSheet.absoluteFill, transform: [{translateY: this.bgY}]}}>
                     <Image source={require('../assets/bg.jpg')}
                            style={{ flex: 1, height: null, width: null}}
                     />
-                </View>
+                </Animated.View>
                 <View style={{height: height/3}}>
                 <TapGestureHandler onHandlerStateChange={this.onStateChange}>
-                    <Animated.View style={{...styles.button, opacity: this.buttonOpacity}}>
+                    <Animated.View style={{...styles.button, 
+                                transform: [{translateY: this.buttonY}],
+                                opacity: this.buttonOpacity}}>
                         <Text style={{ fontSize: 20, fontWeight: "bold"}}>SIGN IN</Text>
                     </Animated.View>
                     </TapGestureHandler>
                 <TapGestureHandler>
-                    <Animated.View style={{...styles.button, backgroundColor: "#2E71DC"}}>
+                    <Animated.View style={{...styles.button, backgroundColor: "#2E71DC", 
+                                    opacity: this.buttonOpacity, transform: [{translateY: this.buttonY}]}}>
                         <Text style={{ fontSize: 20, fontWeight: "bold", color: "white"}}>SIGN IN WITH FACEBOOK</Text>
                     </Animated.View>
                 </TapGestureHandler>
